70149deb875f52aeb2aa999484084b9ca20c15d0
Your Nam <kevinronaldo04m@gmail.com>
2025-10-09 10:34:47 -0500

    Producto Ventas

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/controller/LoginController.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.sysventas.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Controller
+//@Component
+public class LoginController {
+
+
+}
